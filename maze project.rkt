6a9da18e8d7a29f82280c231f34a3503f8573f00
yoniinoy <enatanb@gmail.com>
2018-03-22 20:55:27 +0200

    Update maze project.rkt

--- maze project.rkt
+++ maze project.rkt
@@ -1,19 +1,21 @@
-(define testBoard '((_ _ _ _ _)(_ _ _ _ _)(_ _ _ _ _)(_ _ _ _ _)(_ _ _ _ _)))
-(define testBoardOK '((_ X _ _ _)(_ _ X _ X)(X X _ _ _)(_ _ _ X _)(_ X X X X)))
-(define testBoardNO '((X X _ X _)(X _ X _ X)(X X X _ X)(_ X _ X _)(_ X _ X _)))
-(define sectorTest '((A B C D E)(F G H I J) (K L M N O)(P Q R S T)(U V W X Y)))
+(define testBoard '((_ _ _ _ _)(_ _ _ _ _)(_ _ _ _ _)(_ _ _ _ _)(_ _ _ _ _))) ;an empty 5*5 board
+(define testBoardOK '((_ X _ _ _)(_ _ X _ X)(X X _ _ _)(_ _ _ X _)(_ X X X X))) ;a passible 5*5 maze
+(define testBoardNO '((X X _ X _)(X _ X _ X)(X X X _ X)(_ X _ X _)(_ X _ X _))) ;an unpassible 5*5 maze
+(define sectorTest '((A B C D E)(F G H I J) (K L M N O)(P Q R S T)(U V W X Y))) ;list of letters for PrintSector tests (will be deleted soon (I'm done with the function that needs it))
 
-(define (BoardSize length width)
+;new board creation
+(define (BoardSize length width) ;creates an empty board for the maze to be generated into
   (cond
     ((= width 0) '())
     (else (cons (line length) (BoardSize length (sub1 width))))))
 
-(define (line width)
+(define (line width) ;creates one line of  _ in a given length
   (cond
     ((= width 0) '())
     (else (cons '_ (line (sub1 width))))))
 
-(define (printBoard B)
+;printing section
+(define (printBoard B) ;prints the whole maze (or the full empty board..... it just prints whatever board you give it, OK? now shash!)
    (cond
      ((empty? (rest B)) (print (first B)))
      (else (print (first B))
@@ -31,28 +33,30 @@
   (newline)
   (print (printSectorLine B Xpos (+ Ypos 2) 0)))
 
-(define (printSectorLine B Xpos Ypos counter)
+(define (printSectorLine B Xpos Ypos counter) ;PrintSector helper,
   (cond
     ((= 5 counter) '())
     (else (cons (findTile B (+ Xpos (+ -2 counter)) Ypos) (printSectorLine B Xpos Ypos (add1 counter))))))
 
-(define (findTile B Xpos Ypos)
+(define (findTile B Xpos Ypos) ;returnes a tile in agiven location
   (list-ref (list-ref B Ypos) Xpos))
 
-(define (MazeRandomaizer B)
+;random section
+(define (MazeRandomaizer B) ;fulls the maze with random walls (the maze isnt alwasy passible but I'll fix that later)
   (cond
     ((empty? B) (lineRandomaizer B))
     (else (cons (lineRandomaizer (first B)) (MazeRandomaizer (rest B))))))
 
-(define (lineRandomaizer L)
+(define (lineRandomaizer L) ;MazeRandomaizer helper, runs the RandomTileGenerator for each tile in one line
     (cond
     ((empty? L) '())
     (else (cons (RandomTileGenerator (first L)) (lineRandomaizer (rest L))))))
 
-(define (RandomTileGenerator L)
+(define (RandomTileGenerator L) ;returns a X (wall) or _ (path) tile based on a random number
   (cond
     ((= (random 2) 1) 'X)
     (else '_)))
 
+;auto bord creation and printing
 (define B1 (MazeRandomaizer (BoardSize 10 10)))
 (printBoard B1)
